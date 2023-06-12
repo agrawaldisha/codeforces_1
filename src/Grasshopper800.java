177bd7b6d75bdc6f106d2f808eb0b375f9e43503
Disha <disha.agrawal_cs20@gla.ac.in>
2023-06-12 22:13:02 +0530

    commit

--- src/Grasshopper800.java
+++ src/Grasshopper800.java
@@ -4,23 +4,17 @@ public class Grasshopper800 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        while(t-->0){
-        int a[] = new int[2];
+        while (t-- > 0) {
+            int a[] = new int[2];
             a[0] = sc.nextInt();
             a[1] = sc.nextInt();
-        int temp=a[0];
-        if((a[0]/a[1])<1){
-            System.out.println(1);
-            System.out.println(a[0]);
-        }
-        else{
-            while(a[0]%a[1]!=0){
-                a[0]=a[0]-1;
+            if (a[0] % a[1] != 0) {
+                System.out.println(1);
+                System.out.println(a[0]);
+            } else {
+                System.out.println(2);
+                System.out.println(a[0] - 1 + " " + 1);
             }
-            a[1]=temp-a[0];
-            System.out.println(2);
-            System.out.println(a[0]+" "+a[1]);
-        }
         }
     }
 }
