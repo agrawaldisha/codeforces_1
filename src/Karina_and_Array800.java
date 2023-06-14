9fc4da0ef7b4eb6bac07ab451273de624dd2c03e
Disha <disha.agrawal_cs20@gla.ac.in>
2023-06-14 16:40:53 +0530

    commit

---
+++ src/Karina_and_Array800.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public interface Karina_and_Array800 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            long[] arr = new long[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextLong();
+            }
+            Arrays.sort(arr);
+            System.out.println(Math.max((arr[0] * arr[1]), (arr[n - 1] * arr[n - 2])));
+        }
+    }
+}
