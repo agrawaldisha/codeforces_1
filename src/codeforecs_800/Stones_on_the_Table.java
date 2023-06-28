6302f879ae6faf1746b4099161859ca83b2e6972
Disha <disha.agrawal_cs20@gla.ac.in>
2023-06-28 12:59:16 +0530

    commit

---
+++ src/codeforecs_800/Stones_on_the_Table.java
@@ -0,0 +1,16 @@
+package codeforecs_800;
+
+import java.util.*;
+public class Stones_on_the_Table {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int count=0;
+        char[] arr=sc.next().toCharArray();
+        for(int i=0;i<n-1;i++)
+        {
+            if(arr[i]==arr[i+1])count+=1;
+        }
+        System.out.println(count);
+    }
+}
